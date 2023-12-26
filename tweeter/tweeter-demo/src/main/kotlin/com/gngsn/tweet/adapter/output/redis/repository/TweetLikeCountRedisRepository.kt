096ad1978609a55cdd2da7e936a4e9cf461ce0bd
gngsn <enfj.dev@gmail.com>
2023-12-27 01:51:51 +0900

    Apply using N-th redis DB

--- tweeter/tweeter-demo/src/main/kotlin/com/gngsn/tweet/adapter/output/redis/repository/TweetLikeCountRedisRepository.kt
+++ tweeter/tweeter-demo/src/main/kotlin/com/gngsn/tweet/adapter/output/redis/repository/TweetLikeCountRedisRepository.kt
@@ -1,27 +1,29 @@
 package com.gngsn.tweet.adapter.output.redis.repository
 
+import com.gngsn.tweet.adapter.output.redis.constant.TWEET_LIKE_KEY
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple
 import org.springframework.stereotype.Repository
 
-const val TWEET_LIKE_KEY = "tweet:likes"
 
 @Repository
 class TweetLikeCountRedisRepository(
-    private val redisTemplate: RedisTemplate<String, Any>,
+    private val DB0RedisTemplate: RedisTemplate<String, Any>,
 ) {
+    val zoperation = DB0RedisTemplate.opsForZSet()
+
     fun zadd(tweetId: Long, score: Double) {
-        redisTemplate.opsForZSet().incrementScore(TWEET_LIKE_KEY, tweetId, 1.0)
+        zoperation.incrementScore(TWEET_LIKE_KEY, tweetId, 1.0)
     }
 
     fun findAll(n: Long = -1): Set<ScoreValue> {
-        val reverseRange = redisTemplate.opsForZSet()
+        val reverseRange = zoperation
             .rangeWithScores(TWEET_LIKE_KEY, 0, -1)
         return reverseRange?.map { ScoreValue.of(it) }?.toSet() ?: setOf()
     }
 
     fun remove(tweetId: Long) {
-        redisTemplate.opsForZSet().remove(TWEET_LIKE_KEY, tweetId)
+        zoperation.remove(TWEET_LIKE_KEY, tweetId)
     }
 
     data class ScoreValue(
